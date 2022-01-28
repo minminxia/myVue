006f1def2cd691a4d58ce4a7620c4f516b6159ca
liuminxia <liuminxia@tal.com>
2022-01-28 15:15:24 +0800

    fix: eslint和prettierrc冲突

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // import App from "./App.vue";
 
-import { createApp, defineComponent, h } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { createApp, defineComponent, h } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 // import img from "./assets/logo.png"; // 不符合ts规范
 const img = require("./assets/logo.png"); // eslint-disable-line
@@ -10,16 +10,16 @@ const img = require("./assets/logo.png"); // eslint-disable-line
 // h函数源码：package>runtime-core>src>h.ts
 const App = defineComponent({
   render() {
-    return h('div', { id: 'app' }, [
-      h('img', {
-        alt: 'Vue logo',
+    return h("div", { id: "app" }, [
+      h("img", {
+        alt: "Vue logo",
         src: img,
       }),
       h(HelloWorld, {
-        msg: 'Welcome to Your Vue.js + TypeScript App',
+        msg: "Welcome to Your Vue.js + TypeScript App",
       }),
-    ])
+    ]);
   },
-})
+});
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
